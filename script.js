2a854b5a73bdbf517b33766fee9e8b35818a79fb
furrukht10 <79553858+furrukht10@users.noreply.github.com>
2021-06-02 22:04:58 -0400

    Portfolio section revamp

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ mobileButtonExit.addEventListener("click", () => {
 });
 
 // Overlay + Portfolio
+
+// const btnShowPortfolio = document.querySelector(".show-portfolio");
+
+// for (let i = 0; i < btnShowPortfolio.length; i++) {
+//   btnShowPortfolio[i].addEventListener("click", function () {
+//     document.querySelector(".portfolio-card").classList.toggle("hidden");
+//     console.log(
+//       document.querySelector(".portfolio-card").classList.contains("hidden")
+//     );
+//   });
+// }
