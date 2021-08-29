495106ed51644efbe6cf015a6602dd36adcf0af2
furrukht10 <79553858+furrukht10@users.noreply.github.com>
2021-08-29 13:23:44 -0400

    Temporary Website

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-// Navigation Bar
+window.onscroll = function () {
+  myFunction();
+};
 
-const mobileButton = document.getElementById("openMenu");
-nav = document.querySelector("nav");
-mobileButtonExit = document.getElementById("exitMenu");
+// STICKY NAVBAR
+const header = document.getElementById("myHeader");
 
-mobileButton.addEventListener("click", () => {
-  nav.classList.add("menuButton");
+const sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    header.style.opacity = 0.7;
+  } else {
+    header.classList.remove("sticky");
+    header.style.opacity = 1;
+  }
+}
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
 });
-
-mobileButtonExit.addEventListener("click", () => {
-  nav.classList.remove("menuButton");
-});
-
-// Scroll to top
